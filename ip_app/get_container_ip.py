df86110ae06045863171ffa92775862ad9b7c913
Deepak <120704297+Deepak128404@users.noreply.github.com>
2024-03-21 23:15:28 +0530

    Update get_container_ip.py

--- ip_app/get_container_ip.py
+++ ip_app/get_container_ip.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    import socket
     hostname = socket.gethostname()
     container_ip = socket.gethostbyname(hostname)
     return render_template('index.html', container_ip=container_ip)
