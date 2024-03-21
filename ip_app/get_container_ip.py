2e94dbd11a55fc837e50dfa6b7bd8eca580392f9
Deepak Singh <jitcse.deepaksingh@gmail.com>
2024-03-21 20:20:38 +0530

    flask ip app done

---
+++ ip_app/get_container_ip.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    import socket
+    hostname = socket.gethostname()
+    container_ip = socket.gethostbyname(hostname)
+    return render_template('index.html', container_ip=container_ip)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
